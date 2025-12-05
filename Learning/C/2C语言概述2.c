c61017e47d17a8565388bdbc8a2cb4b5d99d471f
Phorus <littleyuer666@qq.com>
2025-12-05 17:45:54 +0800

    Moved to the right place

---
+++ Learning/C/2C语言概述2.c
@@ -0,0 +1,11 @@
+////2米转化为厘米和分米的程序
+//#include <stdio.h>
+//int main()
+//{
+//	int inum , dm , cm;
+//	inum = 2;
+//	dm = inum * 10;
+//	cm = inum * 100;
+//	printf("2米等于%d分米\n等于%d厘米", dm, cm);
+//	return 0;
+//}
---
+++ Learning/C/2C语言概述3.c
@@ -0,0 +1,19 @@
+////多函数程序
+//#include <stdio.h>
+//void tool();
+//
+//int main()
+//{
+//	int sum;
+//	sum = 1;
+//	printf("我最喜欢的数字是%d\n因为", sum);
+//	tool();
+//	printf("\nPhorus Yu");
+//	return 0;
+//}
+//
+//void tool()
+//{
+//	printf("它是第一");
+//	return 0;
+//}
