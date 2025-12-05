970bd91b664f65332cb4043e59ea36bf641ab878
Phorus <littleyuer666@qq.com>
2025-12-05 17:30:27 +0800

    Add files via upload

---
+++ 2C语言概述1.c
@@ -0,0 +1,10 @@
+////hello world ³ÌÐò
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello world. You will have a great fortune!\n");
+//	int sum;
+//	sum = 20251205;
+//	printf("Phorus Yu\n%d",sum);
+//	return 0;
+//}
